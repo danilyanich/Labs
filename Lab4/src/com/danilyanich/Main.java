2691f1c8514b60ab6e17727f8b706395819ce06f
danilyanich <danilyanich@gmail.com>
2016-10-04 19:25:10 +0300

    main reorganised. Parser usage added(with bugs).

--- Lab4/src/com/danilyanich/Main.java
+++ Lab4/src/com/danilyanich/Main.java
@@ -1,22 +1,78 @@
 package com.danilyanich;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+
 public class Main {
 
     public static void main(String[] args) {
-        Complex complex1 = Complex.rectangular(1,1);
-        Complex complex2 = Complex.polar(1, Math.PI/2);
-        Complex complex3 = Complex.add(complex1, complex2);
-        Complex complex4 = Complex.sub(complex3, complex1.conjugate());
-        complex1 = Complex.div(complex3.div(complex2), complex1);
-        Complex complex5 = complex2.copy().mul(complex4).add(complex4);
-        Complex m_one = Complex.rectangular(0, 1).pow(2);
+        pureviewOperators();
+        testParser();
+    }
 
-        System.out.println(m_one.toString(Complex.Form.RECTANGULAR));
-        System.out.println(complex1.toString(Complex.Form.RECTANGULAR));
-        System.out.println(complex2.toString(Complex.Form.EXPONENTIAL));
-        System.out.println(complex3.toString(Complex.Form.POLAR));
-        System.out.println(complex4.toString(Complex.Form.EXPONENTIAL));
-        System.out.println(complex5.toString(Complex.Form.RECTANGULAR));
+    static void pureviewOperators() {
+        System.out.println("see how operators work:");
+        Complex a = Complex.rectangular(4, 2);
+        Complex b = Complex.polar(1, Math.PI / 4);
+        Complex c = Complex.rectangular(0, 1);
+
+        System.out.println("a:");
+        System.out.println(a.toString(Complex.Form.RECTANGULAR));
+        System.out.println(a.toString(Complex.Form.EXPONENTIAL));
+        System.out.println(a.toString(Complex.Form.POLAR));
+        System.out.println();
+
+        System.out.println("b:");
+        System.out.println(b.toString(Complex.Form.RECTANGULAR));
+        System.out.println(b.toString(Complex.Form.EXPONENTIAL));
+        System.out.println(b.toString(Complex.Form.POLAR));
+        System.out.println();
+
+        System.out.println("c:");
+        System.out.println(c.toString(Complex.Form.RECTANGULAR));
+        System.out.println(c.toString(Complex.Form.EXPONENTIAL));
+        System.out.println(c.toString(Complex.Form.POLAR));
+        System.out.println();
+
+        System.out.println("a + b = " + Complex.add(a, b));
+        System.out.println("b - c = " + Complex.sub(b, c));
+        System.out.println("a * b = " + Complex.mul(a, b));
+        System.out.println("c / b = " + Complex.div(c, b));
+        System.out.println("c^2 = " + c.pow(2));
+        System.out.println();
+    }
+
+    static void testParser() {
+        System.out.println("test complex parser:");
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        List<Complex> list = new ArrayList<>();
+        String string;
+        try {
+            while (!Objects.equals(string = input.readLine(), "")) {
+                Matcher matcher = Complex.PATTERN.matcher(string);
+                while (matcher.find())
+                    try {
+                        list.add(Complex.parseComplex(matcher.group()));
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                        System.out.println();
+                    }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println();
+        }
+        System.out.println();
+        for (Complex c : list) {
+            System.out.println(c.toString() + " ");
+        }
+        System.out.println();
     }
 }
